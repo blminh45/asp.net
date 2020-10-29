2b6c70fff2159ff7bb195d1a1caf1dd31eb1be63
blminh <0306181245@caothang.edu.vn>
2020-10-29 10:05:00 +0700

    create template

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebASP.NetCoreMVCTemplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
